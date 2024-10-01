ba142efa4653afc7bb8b67615846fad3fb117b80
alons0029 <antonio.javier.cabrera@proton.me>
2024-10-01 13:37:03 +0200

    Exercise finished

--- script.js
+++ script.js
@@ -74,70 +74,71 @@ console.log(divideNumber);
 
 //Escribe tu código aquí
 
-
+let var1 = 1;
+let var2 = 2;
+let varComparison = (var1 == var2);
+console.log(varComparison);
 
 
 // Ejercicio 9: Crea otra variable que compare las dos primeras variables creadas en el ejercicio anterior, para que el resultado impreso en la consola sea true.
 
 //Escribe tu código aquí
-
-
+let varComparison2 = (var1 != var2);
+console.log(varComparison2);
 
 
 //Ejercicio 10: completa el ejercicio
-/*
-let num1 = 15
-let num2 = 20
 
-let comparacion = //Realiza la operación de comparación que consideres para que el resultado en consola sea true
+let num1 = 15;
+let num2 = 20;
+
+let comparacion = (num1 != num2);
 console.log(comparacion)
-*/
 
 //Ejercicio 11: completa el ejercicio
-/*
+
 let num3 = 1
 let num3AsString = "1"
 
-let result = //Realiza la operación de comparación que consideres entre las dos variables dadas, para que el resultado en consola sea false sin usar el comparador de desigualdad
-console.log(result) 
-*/
+let result = (typeof(num3) == typeof(num3AsString));
+console.log(result);
+
 
 //Ejercicio 12: completa el ejercicio
-/*
-let result2 = //Realiza la operación de comparación que consideres con las dos variables del ejercicio anterior para que el resultado en consola sea true.
-console.log(result2)
-*/
+
+let result2 = (num3 == num3AsString);
+console.log(result2);
 
 
 //OPERADORES DE CADENAS
 //Ejercicio 13: Crea una variable con tu nombre y otra variable con tu apellido y crea otra nueva variable con tu nombre completo concatenando las dos variables anteriores e imprime en consola. Debe haber un espacio entre tu nombre y apellido.
 
 //Escribe tu código aquí
-
-
-
-
+let firstName = "Antonio"
+let lastName = "Cabrera"
+let myFullName = (firstName + " " + lastName);
+console.log(myFullName);
 
 //OPERADORES DE LÓGICA
 //Ejercicio 14: Descomenta todo el bloque desde la variable 'a' hasta el 'console.log(res6)' y completa el ejercicio. (Saldrá un error y no podrás visualizar el resultado hasta que termines este ejercicio)
 
-/* let a = 6;
+let a = 6;
 let b = 3;
 
-let res =  a < 10 __ b > 1 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
-console.log(res)
+let res =  a < 10 && b > 1;
+console.log(res); // true
 
-let res2 = a < 10 __ b < 1 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea false
-console.log(res2)
+let res2 = a < 10 && b < 1;
+console.log(res2); // false
 
-let res3 = a == 5 __ b == 5 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea false
-console.log(res3)
+let res3 = a == 5 || b == 5;
+console.log(res3); // false
 
-let res4 = a == 6 __ b == 0 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
-console.log(res4)
+let res4 = a == 6 || b == 0;
+console.log(res4); // true
 
-let res5 = a == 0 __ b == 3 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
-console.log(res5)
+let res5 = a == 0 || b == 3;
+console.log(res5); // true
 
-let res6 = a == 6 __ b == 3 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
-console.log(res6) */
+let res6 = a == 6 && b == 3;
+console.log(res6); // true
